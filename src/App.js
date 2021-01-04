2bd246c9cbba79093a2ebba369e0c1e9c17f71c6
Leoncodes00 <chachaiyang3@gmail.com>
2021-01-04 11:58:15 -0600

    second

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends React.Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
   }
 }
 
